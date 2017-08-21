a21627e164556eb3f5227b9255a6d8e4fdd5b97c
Minh Chau <xiu.chau@gmail.com>
2017-08-21 16:12:42 -0700

    prepare for heroku

--- routes/projects.js
+++ routes/projects.js
@@ -5,5 +5,28 @@ const knex = require('knex');
 const { camelizeKeys } = require('humps');
 
 router.get('/projects', (req, res, next) => {
-  
+  knex('projects')
+    .orderBy('created_at', 'desc')
+    .then((projects) => {
+      res.send(camelizeKeys(projects));
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
+
+router.get('/projects/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  knex('projects')
+    .where('id', id)
+    .first()
+    .then((project) => {
+      res.send(camelizeKeys(project));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
