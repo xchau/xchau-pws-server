5347f1fa37d04de02fa09bb366e2b2998fcda135
Minh Chau <xiu.chau@gmail.com>
2017-09-07 16:09:02 -0700

    test sendfile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') {
@@ -19,6 +20,20 @@ if (process.env.NODE_ENV !== 'production') {
 const projects = require('./routes/projects');
 
 app.use('/api', projects);
+
+app.get('/resume', (req, res, next) => {
+  const resumePath = path.join(__dirname, 'docs/chau-minh-resume.pdf');
+
+  res.writeHead({
+    'Content-Type': 'application/pdf'
+  });
+
+  // const readStream = fileSystem.createReadStream(resumePath);
+
+  // readStream.pipe(res);
+
+  res.sendFile(resumePath);
+});
 
 app.use((req, res) => {
   res.sendStatus(404);
