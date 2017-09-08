fc578b503a7af5f1bee681c35785411a0772c34b
Minh Chau <xiu.chau@gmail.com>
2017-09-08 15:49:11 -0700

    add access-control-allow-origin

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ if (process.env.NODE_ENV !== 'production') {
   const morgan = require('morgan');
 
   app.use(morgan('short'));
-
-  app.use('/', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
 }
+
+app.use('/', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const projects = require('./routes/projects');
 
